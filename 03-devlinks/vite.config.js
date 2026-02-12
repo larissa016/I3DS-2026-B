bb173f0322dce7cff83f30e08628e33a966fcb90
larissa016 <noguerolarissa@gmail.com>
2026-02-12 11:30:35 -0300

    feat: reestruturar componentes e remover estilos desnecessários

--- 03-devlinks/vite.config.js
+++ 03-devlinks/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,//muda a porta padrão Vite(5173)para a padrão react(3000)
+    open: true,//abre o navegador automaticamente
+    host: true,//permite acesso via IP de rede local
+  },
 })
