8a2c088b8683222a48a0f1b8b04d9bb6a3455e4c
larissa016 <noguerolarissa@gmail.com>
2026-02-13 10:40:38 -0300

    Implement code structure updates and remove redundant sections

--- 03-devlinks/vite.config.js
+++ 03-devlinks/vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,//muda a porta padrão Vite(5173)para a padrão react(3000)
-    open: true,//abre o navegador automaticamente
-    host: true,//permite acesso via IP de rede local
+    port: 3000, //muda a porta padrão Vite(5173)para a padrão react(3000)
+    open: true, //abre o navegador automaticamente
+    host: true, //permite acesso via IP de rede local
   },
-})
+});
