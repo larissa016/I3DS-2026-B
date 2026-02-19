1c303f96037b8610b6e88664549cae3631846e8c
larissa016 <noguerolarissa@gmail.com>
2026-02-19 11:18:31 -0300

    feat: adicionar componente Rodape e estilização correspondente

--- 04-devflix/src/App.jsx
+++ 04-devflix/src/App.jsx
@@ -2,12 +2,19 @@ import "./App.css";
 
 
 import logo from "./assets/devflix.png"
-import lupa from "./asset/search.svg"
+import lupa from "./assets/search.svg"
+import Rodape from "./Componentes/Rodape/Rodape";
+
 const App = () => {
   return (
-    <div>App
+    <div id="App">
       <img id="logo"src={logo} alt="Logotipo da plataforma de streaming DEVFLIX em destaque, com letras vermelhas sobre fundo preto, representando conteúdo de entretenimento online."/>
-    <div className="sea">
+    <div className="search">
+      <input type="text" placeholder="Pesquise por filme e serie..."/>
+      <img src={lupa} alt="Botão  de ação para pesquisa!"/>
+ 
+    </div>
+    <Rodape>Larissa</Rodape>
     </div>
   )
 }
